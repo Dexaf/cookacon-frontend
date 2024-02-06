8940ed9ae8694e8eaaa146f7b0c0ed4a82be02a9
Dexaf <federicopisajob@gmail.com>
2024-02-07 00:46:59 +0100

    first set up completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./layout/nav-bar/nav-bar.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, NavBarComponent]
 })
 export class AppComponent {
-  title = 'cookacon-frontend';
 }
