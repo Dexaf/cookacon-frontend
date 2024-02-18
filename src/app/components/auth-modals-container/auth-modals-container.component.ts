a5ad294ec6b2ee8fac9d17a85c7e03a89cbb4b6a
Dexaf <federicopisajob@gmail.com>
2024-02-19 00:19:04 +0100

    added form logic to signin form

--- src/app/components/auth-modals-container/auth-modals-container.component.ts
+++ src/app/components/auth-modals-container/auth-modals-container.component.ts
@@ -7,6 +7,8 @@ import { ModalWrapperComponent } from '../modal-wrapper/modal-wrapper.component'
 import { IconsModule } from '../../icons.module';
 import { CommonModule } from '@angular/common';
 import { TranslocoModule } from '@ngneat/transloco';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { arePasswordsEquals, isStrongPassword } from './signInForm-validators';
 
 @Component({
   selector: 'app-auth-modals-container',
@@ -15,16 +17,31 @@ import { TranslocoModule } from '@ngneat/transloco';
     ModalWrapperComponent,
     IconsModule,
     CommonModule,
-    TranslocoModule
+    TranslocoModule,
+    ReactiveFormsModule
   ],
   templateUrl: './auth-modals-container.component.html',
   styleUrls: ['./auth-modals-container.component.scss', '../../../styles.scss']
 })
 export class AuthModalsContainerComponent {
+  //services
   store = inject(Store<AppState>)
+  formBuilder = inject(FormBuilder)
+  //vars
   isLoginModalOpened$ = this.store.select(storeSelectors.isLoginModalOpened$);
   isSigninModalOpened$ = this.store.select(storeSelectors.isSigninModalOpened$);
 
+  signInForm = this.formBuilder.group({
+    email: ['', [Validators.email, Validators.required]],
+    username: ['', [Validators.minLength(4), Validators.required]],
+    password: ['', [isStrongPassword, Validators.required]],
+    passwordCopy: ['', [Validators.required]]
+  },
+  {
+      validators: arePasswordsEquals
+  })
+
+  //methods
   closeLoginModal() {
     this.store.dispatch(closeLoginModal());
   }
---
+++ src/app/components/auth-modals-container/signInForm-validators.ts
@@ -0,0 +1,33 @@
+import { AbstractControl, FormControl, ValidationErrors } from "@angular/forms"
+
+
+export const isStrongPassword = (controlForm: FormControl): ValidationErrors | null => {
+  const password: string = controlForm.value;
+
+  const isStrongPasswordResults = {
+    hasMinLength: password.length >= 6,
+    hasSpecialCharacters: /.*[!@#$%^&*()_+{}\[\]:;<>,.?\/\\`~\-='"].*/.test(password),
+    hasNumbers: /.*[0-9].*/.test(password),
+    hasUpperCase: /.*[A-Z].*/.test(password),
+    hasLowerCase: /.*[a-z].*/.test(password)
+  }
+
+  if (isStrongPasswordResults.hasMinLength &&
+    isStrongPasswordResults.hasSpecialCharacters &&
+    isStrongPasswordResults.hasNumbers &&
+    isStrongPasswordResults.hasUpperCase &&
+    isStrongPasswordResults.hasLowerCase)
+    return null;
+  else
+    return isStrongPasswordResults
+}
+
+export const arePasswordsEquals = (group: AbstractControl): ValidationErrors | null => {
+  debugger
+  const pass: string | null = group.get('password')?.value;
+  const confirmPass: string | null = group.get('passwordCopy')?.value
+  if (pass !== null && confirmPass !== null && pass === confirmPass)
+    return null
+  else
+    return { passwordAreEquals: false }
+}
