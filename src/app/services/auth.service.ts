c4e82dc4c7f4a91e6d5eee08520b994e544e1d1e
Dexaf <federicopisajob@gmail.com>
2024-03-01 01:08:08 +0100

    added routine to handle auth token with cookies

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID, inject } from '@angular/core';
 import { logInDtoOut, signInDtoOut } from '../models/dtos/out/auth.dto.out.interface';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
@@ -7,12 +7,15 @@ import { logInDtoIn, signInDtoIn } from '../models/dtos/in/auth.dto.in.interface
 import { Store } from '@ngrx/store';
 import { AppState } from '@store/interfaces';
 import { addAuthToken } from '@store/actions';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   //services
   private readonly http = inject(HttpClient);
   private readonly store = inject(Store<AppState>);
+
+  constructor(@Inject(DOCUMENT) private readonly document: Document, @Inject(PLATFORM_ID) private readonly platformId: Object){}
 
   //methods
   signIn(userData: signInDtoOut): Observable<signInDtoIn> {
@@ -30,15 +33,17 @@ export class AuthService {
     const day = new Date();
     //make it tomorrow date
     day.setDate(day.getDate() + 1);
-    document.cookie = `__cookacon__authToken=${authToken};expires=${day.toUTCString()}`;
+    this.document.cookie = `__cookacon__authToken=${authToken};expires=${day.toUTCString()}`;
   }
 
   loadAuthTokenFromCookie() {
-    const authTokenCookie = document.cookie.split(";").find(cookies => cookies.includes("__cookacon__authToken"));
-    if (authTokenCookie) {
-      const authToken = authTokenCookie.split("=")[1];
-      if (authToken)
-        this.store.dispatch(addAuthToken({ authToken }));
+    if(isPlatformBrowser(this.platformId)) {
+      const authTokenCookie = this.document.cookie.split(";").find(cookies => cookies.includes("__cookacon__authToken"));
+      if (authTokenCookie) {
+        const authToken = authTokenCookie.split("=")[1];
+        if (authToken)
+          this.store.dispatch(addAuthToken({ authToken }));
+      }
     }
   }
 }
