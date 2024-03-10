003cebefbfd8cc283ef8faa3af8fa2dcb5f01d9c
Dexaf <federicopisajob@gmail.com>
2024-03-10 12:51:11 +0100

    creata scheda ricetta

--- src/app/components/recipes-list/recipes-list.component.ts
+++ src/app/components/recipes-list/recipes-list.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FeedService } from '../../services/feed.service';
 import { Recipe } from '../../models/interfaces/feed.interface';
 import { environment } from '../../../environments/environment';
+import { Subscription } from 'rxjs';
+import { IconsModule } from '../../icons.module';
+import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-recipes-list',
   standalone: true,
-  imports: [],
+  imports: [
+    IconsModule,
+    CommonModule,
+    TranslocoModule
+  ],
   templateUrl: './recipes-list.component.html',
-  styleUrl: './recipes-list.component.scss'
+  styleUrls: ['./recipes-list.component.scss', '../../../styles.scss']
 })
-export class RecipesListComponent implements OnInit {
+export class RecipesListComponent implements OnInit, OnDestroy {
   feedService = inject(FeedService)
   recipes: Recipe[] = [];
   providerUrl: string = "";
+  subscriptions: Subscription[] = [];
 
   ngOnInit() {
     this.providerUrl = environment.providerUrl;
-    this.feedService.getOwnRecipes(0)
-      .subscribe(recipes => {
-        this.recipes = recipes
-      })
+
+    this.subscriptions.push(
+      this.feedService.getOwnRecipes(0)
+        .subscribe(recipes => {
+          this.recipes = recipes
+        })
+    )
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 }
--- src/app/icons.module.ts
+++ src/app/icons.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatherModule } from 'angular-feather';
-import { Key, Mail, Repeat, User, X } from 'angular-feather/icons';
+import { Calendar, Clock, Coffee, Key, Mail, Repeat, User, Users, X } from 'angular-feather/icons';
 
 const icons = {
   X,
   Mail,
   User,
   Key,
-  Repeat
+  Repeat,
+  Calendar,
+  Clock,
+  Coffee,
+  Users
 }
 
 @NgModule({
