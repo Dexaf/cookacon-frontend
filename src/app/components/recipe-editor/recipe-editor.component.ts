1ca9f59d0cce784603faf559b24764cba8e21178
Dexaf <federicopisajob@gmail.com>
2024-03-23 00:11:12 +0100

    made form for steps and ingrd

--- src/app/components/recipe-editor/recipe-editor.component.ts
+++ src/app/components/recipe-editor/recipe-editor.component.ts
@@ -4,14 +4,14 @@ import { IconsModule } from '../../icons.module';
 import { TranslocoModule } from '@ngneat/transloco';
 import { RecipeType } from '../../models/enums/recipeTypes.enum';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ImageUploadComponent } from '../image-upload/image-upload.component';
+import { customFile, ImageUploadComponent } from '../image-upload/image-upload.component';
 
 @Component({
   selector: 'app-recipe-editor',
   standalone: true,
-  imports: [IconsModule, TranslocoModule, CommonModule, ReactiveFormsModule, ImageUploadComponent],
+  imports: [IconsModule, TranslocoModule, CommonModule, ReactiveFormsModule, ImageUploadComponent, FormsModule],
   templateUrl: './recipe-editor.component.html',
   styleUrls: ['./recipe-editor.component.scss', '../../../styles.scss']
 })
@@ -25,6 +25,7 @@ export class RecipeEditorComponent implements OnChanges {
 
   recipeTypes = RecipeType;
 
+  //Recipe form
   recipeForm = this.formGroup.group({
     mainPicture: [null, [Validators.required]],
     title: ['', [Validators.required]],
@@ -34,12 +35,71 @@ export class RecipeEditorComponent implements OnChanges {
     cookingTimeMinute: [0, [Validators.required, Validators.min(0), Validators.max(59)]],
     type: ["ND", [Validators.required]],
   })
-  isEditing: boolean = false;
+
+  //ingredient form
+  ingredientForm = this.formGroup.group({
+    ingredientName: [null, [Validators.required]],
+    ingredientQta: [null, [Validators.required]],
+    ingredientPicture: [false, [Validators.requiredTrue]],
+  })
+  ingredientPicture: customFile | null = null;
+
+  //steps form
+  stepsForm = this.formGroup.group({
+    stepTitle: [null, [Validators.required]],
+    stepDescription: [null, [Validators.required]],
+    stepPicture: [false, [Validators.requiredTrue]],
+  })
+  stepPicture: customFile | null = null;
 
   ngOnChanges(changes: SimpleChanges) {
     if (this.editMode && this.recipeData) {
       this.loadRecipeData(this.recipeData)
     };
+  }
+
+  loadRecipeData(recipeData: Recipe) {
+    //TODO - pass data to app-image-upload
+    //this.recipeForm.controls.mainPicture.setValue(this.recipeData?.mainPictureUrl ?? null);
+    this.recipeForm.controls.title.setValue(recipeData?.title ?? null);
+    this.recipeForm.controls.description.setValue(recipeData?.description ?? null);
+    this.recipeForm.controls.minQta.setValue(recipeData?.minQta ?? 1);
+
+    const times = recipeData?.cookingTime.split(":");
+
+    if (times && times.length > 0) {
+      this.recipeForm.controls.cookingTime.setValue(parseInt(times[0]) ?? 0);
+      this.recipeForm.controls.cookingTimeMinute.setValue(parseInt(times[1]) ?? 0);
+    }
+    const type = Object.keys(this.recipeTypes).find(v => v === recipeData.type)
+    this.recipeForm.controls.type.setValue(type ?? "ND");
+  }
+
+  getIngredientUploadedPhoto(uploadedPictures: customFile[]) {
+    if (uploadedPictures[0]) {
+      this.ingredientPicture = uploadedPictures[0];
+      this.ingredientForm.controls.ingredientPicture.setValue(true)
+    } else {
+      this.ingredientPicture = null;
+      this.ingredientForm.controls.ingredientPicture.setValue(false)
+    }
+  }
+
+  getStepUploadedPhoto(uploadedPictures: customFile[]) {
+    if (uploadedPictures[0]) {
+      this.stepPicture = uploadedPictures[0];
+      this.stepsForm.controls.stepPicture.setValue(true)
+    } else {
+      this.stepPicture = null;
+      this.stepsForm.controls.stepPicture.setValue(false)
+    }
+  }
+
+  addStep() {
+    throw new Error('Method not implemented.');
+  }
+  addIngredient() {
+    throw new Error('Method not implemented.');
   }
 
   submitRecipeEditForm() {
@@ -62,21 +122,5 @@ export class RecipeEditorComponent implements OnChanges {
       }
 
     return true;
-  }
-
-  loadRecipeData(recipeData: Recipe) {
-    //this.recipeForm.controls.mainPicture.setValue(this.recipeData?.mainPictureUrl ?? null);
-    this.recipeForm.controls.title.setValue(recipeData?.title ?? null);
-    this.recipeForm.controls.description.setValue(recipeData?.description ?? null);
-    this.recipeForm.controls.minQta.setValue(recipeData?.minQta ?? 1);
-
-    const times = recipeData?.cookingTime.split(":");
-
-    if (times && times.length > 0) {
-      this.recipeForm.controls.cookingTime.setValue(parseInt(times[0]) ?? 0);
-      this.recipeForm.controls.cookingTimeMinute.setValue(parseInt(times[1]) ?? 0);
-    }
-    const type = Object.keys(this.recipeTypes).find(v => v === recipeData.type)
-    this.recipeForm.controls.type.setValue(type ?? "ND");
   }
 }
